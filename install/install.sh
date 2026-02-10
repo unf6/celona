ca32d270ae1f9cd09b0388d542b4f7cdca6717d4
Xretic <95174481+unf6@users.noreply.github.com>
2026-02-10 17:42:17 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -2,9 +2,27 @@
 set -Eeuo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+INSTALL_DIR="$HOME/.config/quickshell/celona"
+SRC_SHADERS_DIR="$INSTALL_DIR/shaders"
+DST_SHADERS_DIR="$SRC_SHADERS_DIR/qsb"
+NAMES=("parallax.vert" "parallax.frag")
+source "$ROOT_DIR/pkg.sh"
 
 BLUE="\033[34m"
 RESET="\033[0m"
+
+compile_shaders() {
+    info "Compiling shaders..."
+    mkdir -p "$DST_SHADERS_DIR"
+
+    for shader in "${NAMES[@]}"; do
+        qsb --qt6 \
+            -o "$DST_SHADERS_DIR/$shader.qsb" \
+            "$SRC_SHADERS_DIR/$shader"
+    done
+
+    ok "Shader compilation done"
+}
 
 clear
 echo -e "${BLUE}"
@@ -21,8 +39,11 @@ echo -e "${RESET}"
 
 bash "$ROOT_DIR/pkg.sh"
 
-mkdir -p ~/.config/quickshell/celona
-cp -r ../quickshell/celona/* ~/.config/quickshell/celona
+info "Installing Celona config"
+mkdir -p "$INSTALL_DIR"
+cp -r "$ROOT_DIR/../quickshell/celona/"* "$INSTALL_DIR"
+ 
+compile_shaders
 
-echo "Finished!"
-echo "Check ~/.config/quickshell/nucleus-shell/* exists to confirm installation."
+ok "Finished!"
+info "Check ~/.config/quickshell/nucleus-shell/* exists to confirm installation."
